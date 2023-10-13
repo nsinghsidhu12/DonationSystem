fa38131938b3eaeaacb0fd9d0089ab284f105fef
Noorarjun Sidhu <nsidhu73@my.bcit.ca>
2023-10-13 01:44:28 -0700

    more style changes

---
+++ ClassLibDb/ViewModels/Report.cs
@@ -0,0 +1,13 @@
+using ClassLibDb.Models;
+
+namespace ClassLibDb.ViewModels
+{
+    public class Report
+    {
+        public string? Title { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<Donation>? Donations { get; set; }
+        public string? ContactFullName { get; set; }
+        public DateTime DonationDate { get; set; }
+    }
+}
--- MvcApp/Controllers/YearlyReportController.cs
+++ MvcApp/Controllers/YearlyReportController.cs
@@ -1,49 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
-using ClassLibDb.Data; 
-using ClassLibDb.Models;
+using ClassLibDb.Data;
+using ClassLibDb.ViewModels;
 using Microsoft.EntityFrameworkCore;
 
-public class ReportController : Controller
+namespace MvcApp.Controllers
 {
-    private readonly ApplicationDbContext _context;
-
-    public ReportController(ApplicationDbContext context)
+    public class ReportController : Controller
     {
-        _context = context;
-    }
-	    public IActionResult Index()
-    {
-            // Fetch data from your data source and pass it to the view
-    var donations = _context.Donations.Include(d => d.Contact)
-                                      .Include(d => d.TransactionType)
-                                      .Include(d => d.PaymentMethod)
-                                      .ToList();
+        private readonly ApplicationDbContext _context;
 
-    return View(donations);
-    }
-
-
- 
-public IActionResult YearlyReport()
-{
-    var yearlyDonations = _context.Donations
-        .Include(d => d.Contact)
-        .Where(d => d.Date.Year == DateTime.Now.Year)
-        .GroupBy(d => new { d.Date.Year, d.Contact.AccountNo })
-        .Select(group => new ReportViewModel
+        public ReportController(ApplicationDbContext context)
         {
-            Title = $"Yearly Report - {group.Key.Year}",
-            TotalAmount = (decimal)group.Sum(d => d.Amount),
-            Donations = group.ToList(),
-            ContactFullName = group.First().Contact.FullName
-        })
-        .ToList();
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // Fetch data from your data source and pass it to the view
+            var donations = _context.Donations.Include(d => d.Contact)
+                                              .Include(d => d.TransactionType)
+                                              .Include(d => d.PaymentMethod)
+                                              .ToList();
 
-    return View("YearlyReportView", yearlyDonations);
-}
+            return View(donations);
+        }
 
 
 
+        public IActionResult YearlyReport()
+        {
+            var yearlyDonations = _context.Donations
+                .Include(d => d.Contact)
+                .Where(d => d.Date.Year == DateTime.UtcNow.Year)
+                .GroupBy(d => new { d.Date.Year, d.Contact.AccountNo })
+                .Select(group => new Report
+                {
+                    Title = $"Yearly Report - {group.Key.Year}",
+                    TotalAmount = (decimal)group.Sum(d => d.Amount),
+                    Donations = group.ToList(),
+                    ContactFullName = group.First().Contact.FullName
+                })
+                .ToList();
+
+            return View("YearlyReportView", yearlyDonations);
+        }
+    }
 }
