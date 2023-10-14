ab113a7e81ad2f95d393a34c975183f1e549f439
atsuki <csatsfapsfu2930@gmail.com>
2023-10-13 19:39:57 -0700

    deleted comments

--- MvcApp/Controllers/YearlyReportController.cs
+++ MvcApp/Controllers/YearlyReportController.cs
@@ -48,20 +48,6 @@ public IActionResult YearlyReport(){
         
 public IActionResult YearToDateReport()
 {
-    // Group donations by year and calculate total amount for each year
-    // var yearlyDonations = _context.Donations
-    //     .Where(d => d.Date.Year == DateTime.UtcNow.Year)
-    //     .GroupBy(d => d.Date.Year)
-    //     .Select(group => new Report
-    //     {
-    //         Title = $"Yearly Report - {group.Key}",
-    //         TotalAmount = (decimal)group.Sum(d => d.Amount),
-    //         Year = group.Key
-    //     })
-    //     .OrderByDescending(r => r.Year)
-    //     .ToList();
-
-    // Year-To-Date Donation Report (from January 1 to now)
     var yearToDateDonations = _context.Donations
         .Include(d => d.Contact)
         .Where(d => d.Date.Year == DateTime.UtcNow.Year && d.Date <= DateTime.UtcNow)
@@ -75,8 +61,6 @@ public IActionResult YearToDateReport()
             DonationDate = group.First().Date
         })
         .ToList();
-
-    // var combinedReports = yearlyDonations.Concat(yearToDateDonations).ToList();
 
     return View(yearToDateDonations);
 }
